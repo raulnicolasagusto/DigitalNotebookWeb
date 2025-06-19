a9bdae3bee222701411fb1d65320b04380d3f5d8
Raul Agusto <raulnicolasagusto@gmail.com>
2025-06-19 16:15:12 -0300

    error en tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+export default {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+  corePlugins: {
+    preflight: true,
+  },
+}
